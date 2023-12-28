3707e1cac5c4c4bd1e0746fb0b39228171237928
Motion of Knowledge <155095830+motionofknowledge@users.noreply.github.com>
2023-12-28 23:36:40 +0530

    Delete server-route.js

--- server-route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { uploadFile } from '../server-upload-api.js';
-const router = express.Router();
-
-//File Upload
-
-router.post('/upload-image', uploadFile);
-
-//File Upload
